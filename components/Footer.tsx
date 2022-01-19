abb99bf580527ccf132c8225263187d96a0d8f8a
Vidarshan <vidarshanadithya3@gmail.com>
2022-01-19 19:37:05 +0530

    Added skeleton

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import { Col, Grid } from "@mantine/core";
+
+const Footer = () => {
+  return (
+    <Grid>
+      <Col sx={{ backgroundColor: "green" }} span={6}>
+        dd
+      </Col>
+      <Col sx={{ backgroundColor: "yellow" }} span={6}>
+        dd
+      </Col>
+    </Grid>
+  );
+};
+
+export default Footer;
