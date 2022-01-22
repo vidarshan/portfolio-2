b489fa1605cf7b7c9f41da74b273cc2b8e2dcbe1
Vidarshan <vidarshanadithya3@gmail.com>
2022-01-22 13:48:46 +0530

    Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,11 @@ import {
   Affix,
   Transition,
   Divider,
+  Card,
+  TextInput,
+  Button,
+  Anchor,
+  Group,
 } from "@mantine/core";
 import type { NextPage } from "next";
 import { useState } from "react";
@@ -18,8 +23,11 @@ import { useWindowScroll } from "@mantine/hooks";
 import {
   BsMoonStarsFill,
   BsSunFill,
-  BsDashCircleFill,
+  BsLinkedin,
   BsCheckCircleFill,
+  BsGithub,
+  BsStackOverflow,
+  BsTwitter,
 } from "react-icons/bs";
 import { FiArrowUp } from "react-icons/fi";
 import { MantineProvider } from "@mantine/core";
@@ -29,6 +37,7 @@ import ProjectCard from "../components/ProjectCard";
 import Header from "../components/Header";
 import About from "../components/About";
 import Work from "../components/Work";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const [scroll, scrollTo] = useWindowScroll();
@@ -127,7 +136,58 @@ const Home: NextPage = () => {
                 </Grid>
               </Col>
               <Col sx={{ height: "80vh" }} span={12}>
-                dd
+                <Divider label="Reach Out" />
+                <Grid>
+                  <Col span={9}>
+                    <Card>
+                      <Col span={12}>
+                        <Divider label="Send me a message" />
+                      </Col>
+                      <Col span={12}>
+                        <TextInput label="Your email" required />
+                      </Col>
+                      <Col span={12}>
+                        <TextInput label="Message" />
+                      </Col>
+                      <Col sx={{ marginTop: "1rem" }} span={12}>
+                        <Button variant="filled" color="dark" fullWidth>
+                          Send Message
+                        </Button>
+                      </Col>
+                    </Card>
+                  </Col>
+                  <Col span={3}>
+                    <Card>
+                      <Col span={12}>
+                        <Divider label="My socials" />
+                      </Col>
+                      <Col span={12}>
+                        <Group>
+                          <BsLinkedin />
+                          <Text weight={500}>Linkedin</Text>
+                        </Group>
+                      </Col>
+                      <Col span={12}>
+                        <Group>
+                          <BsGithub />
+                          <Text weight={500}>Github</Text>
+                        </Group>
+                      </Col>
+                      <Col span={12}>
+                        <Group>
+                          <BsStackOverflow />
+                          <Text weight={500}>Stackoverflow</Text>
+                        </Group>
+                      </Col>
+                      <Col span={12}>
+                        <Group>
+                          <BsTwitter />
+                          <Text weight={500}>Twitter</Text>
+                        </Group>
+                      </Col>
+                    </Card>
+                  </Col>
+                </Grid>
               </Col>
             </Grid>
             <Footer />
