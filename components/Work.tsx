2adca27896993670ae267b182628c6c3e8f63113
Vidarshan Rathnayake <vidarshanadithya3@gmail.com>
2022-05-07 12:24:46 +0530

    Added new work experience layout

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { Text, Timeline, Card, Badge, Group, List, Space } from "@mantine/core";
 import { BsCheckCircle, BsDash, BsDashCircle, BsDashLg } from "react-icons/bs";
-import { BiCalendar, BiData } from "react-icons/bi";
+import { BiCalendar, BiData, BiHistory } from "react-icons/bi";
 
 const Work = () => {
   return (
     <Card className="content-spacing" withBorder radius="xs">
-      <Timeline
-        color="gray"
-        active={3}
-        bulletSize={14}
-        lineWidth={3}
-        radius="xs"
-      >
+      <Timeline color="gray" bulletSize={14} lineWidth={3} radius="xs">
         <Timeline.Item
           bullet={<BsDash />}
           align="right"
@@ -26,7 +20,7 @@ const Work = () => {
           >
             Excalibur Systems, Sri Lanka
           </Text>
-          <List size="sm" mt={30}>
+          <List spacing="xs" size="sm" center icon={<BiHistory />} mt={30}>
             <List.Item>
               Contributed to existing web applications based on React JS, to
               extend and improve their functionality.
@@ -60,7 +54,6 @@ const Work = () => {
               Javascript
             </Badge>
           </Group>
-
           <Group position="left" direction="row">
             <BiCalendar />
             <Text weight={600} className="row-bottom-top-spacing" size="xs">
@@ -81,11 +74,24 @@ const Work = () => {
           >
             Beta Launch, Sri Lanka
           </Text>
-          <Text weight={700} color="dimmed" size="sm">
-            Worked as a frontend software engineer and implemented new
-            functionalities to existing projects along with replacing old and
-            outdated user interfaces to modern user-friendly user interfaces.
-          </Text>
+          <List spacing="xs" size="sm" center icon={<BiHistory />} mt={30}>
+            <List.Item>
+              Revamped web applications with old user interfaces to modern and
+              updated user interfaces.
+            </List.Item>
+            <List.Item>
+              Implemented and validated new functionalities on frontend in React
+              JS based web applications.
+            </List.Item>
+            <List.Item>
+              Modified and improved backend REST API's functialities.
+            </List.Item>
+            <List.Item>
+              Modified relational database's structures to cater new
+              functionalities.
+            </List.Item>
+          </List>
+          <Space h="md" />
           <Group className="row-bottom-top-spacing">
             <Badge size="md" variant="dot" radius="xs">
               React JS
@@ -96,20 +102,21 @@ const Work = () => {
             <Badge size="md" color="pink" variant="dot" radius="xs">
               SCSS
             </Badge>
+            <Badge size="md" color="green" variant="dot" radius="xs">
+              Node JS
+            </Badge>
+            <Badge size="md" color="gray" variant="dot" radius="xs">
+              Express JS
+            </Badge>
           </Group>
-          <Group>
-            <BiData size="10px" />
+          <Group position="left" direction="row">
+            <BiCalendar />
             <Text weight={500} className="row-bottom-top-spacing" size="xs">
               04-2021 &#8212; 09-2021
             </Text>
           </Group>
         </Timeline.Item>
-
-        <Timeline.Item
-          bullet={<BsDash />}
-          title="Associate Software Engineer"
-          lineVariant="dashed"
-        >
+        <Timeline.Item bullet={<BsDash />} title="Associate Software Engineer">
           <Text
             className="row-bottom-top-spacing-half"
             weight={600}
@@ -118,12 +125,24 @@ const Work = () => {
           >
             Beta Launch, Sri Lanka
           </Text>
-          <Text weight={700} color="dimmed" size="sm">
-            Worked as a full-stack software engineer, implemented new
-            functionalities along with improving existing functionalities in
-            special purpose web applications. Gained knowledge on end-to-end
-            testing frameworks for web applications.
-          </Text>
+          <List spacing="xs" size="sm" center icon={<BiHistory />} mt={30}>
+            <List.Item>
+              Gained knowledge and implemented end-to-end testing for web
+              applications.
+            </List.Item>
+            <List.Item>
+              Implemented and validated new functionalities on frontend in React
+              JS based web applications.
+            </List.Item>
+            <List.Item>
+              Modified and improved backend REST API's functialities.
+            </List.Item>
+            <List.Item>
+              Modified relational database's structures to cater new
+              functionalities.
+            </List.Item>
+          </List>
+          <Space h="md" />
           <Group className="row-bottom-top-spacing">
             <Badge size="md" variant="dot" radius="xs">
               React JS
@@ -141,9 +160,12 @@ const Work = () => {
               MySQL
             </Badge>
           </Group>
-          <Text weight={500} className="row-bottom-top-spacing" size="xs">
-            09-2021 &#8212; 03-2022
-          </Text>
+          <Group position="left" direction="row">
+            <BiCalendar />
+            <Text weight={500} className="row-bottom-top-spacing" size="xs">
+              09-2021 &#8212; 03-2022
+            </Text>
+          </Group>
         </Timeline.Item>
         <Timeline.Item bullet={<BsDash />} title="Associate Software Engineer">
           <Text
@@ -152,13 +174,23 @@ const Work = () => {
             color="blue"
             size="sm"
           >
-            Dev4s
+            Dev4s, Sri Lanka
           </Text>
-          <Text weight={700} color="dimmed" size="sm">
-            Working as a full-stack software developer for applications based on
-            MERN stack. Heavy on frontend, implementing User Interfaces based on
-            low-level prototypes to the production.
-          </Text>
+          <List spacing="xs" size="sm" center icon={<BiHistory />} mt={30}>
+            <List.Item>
+              Designed user interfaces with Figma and implemented those with
+              code.
+            </List.Item>
+            <List.Item>
+              Implemented new functionalities per client requirements both in
+              the frontend and the backend.
+            </List.Item>
+            <List.Item>
+              Worked with 3rd party APIs such as Google API to fetch necessary
+              information for web applications.
+            </List.Item>
+          </List>
+          <Space h="md" />
           <Group className="row-bottom-top-spacing">
             <Badge size="md" variant="dot" radius="xs">
               React JS
@@ -172,10 +204,51 @@ const Work = () => {
             <Badge size="md" color="yellow" variant="dot" radius="xs">
               Firebase
             </Badge>
+            <Badge size="md" color="pink" variant="dot" radius="xs">
+              Figma
+            </Badge>
           </Group>
-          <Text weight={500} className="row-bottom-top-spacing" size="xs">
-            03-2022 &#8212; Present
+          <Group position="left" direction="row">
+            <BiCalendar />
+            <Text weight={500} className="row-bottom-top-spacing" size="xs">
+              03-2022 &#8212; 04-2022
+            </Text>
+          </Group>
+        </Timeline.Item>
+        <Timeline.Item
+          bullet={<BsDash />}
+          title="Software Engineer"
+          lineVariant="dotted"
+        >
+          <Text
+            className="row-bottom-top-spacing-half"
+            weight={600}
+            color="blue"
+            size="sm"
+          >
+            Appspotr
           </Text>
+          <Space h="md" />
+          <Text weight={500} size="sm">
+            Contributing to build a low-code mobile application building
+            platform, which allows users to create mobile applications with
+            great convenience and ease.
+          </Text>
+          <Space h="md" />
+          <Group className="row-bottom-top-spacing">
+            <Badge size="md" variant="dot" radius="xs">
+              React JS
+            </Badge>
+            <Badge size="md" color="blue" variant="dot" radius="xs">
+              React Native
+            </Badge>
+          </Group>
+          <Group position="left" direction="row">
+            <BiCalendar />
+            <Text weight={500} className="row-bottom-top-spacing" size="xs">
+              05-2022 &#8212; Present
+            </Text>
+          </Group>
         </Timeline.Item>
       </Timeline>
     </Card>
